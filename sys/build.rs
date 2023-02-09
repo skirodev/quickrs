975e695b60a4946e375b605d264541fb7d059cd7
skirodev <sinpx@foxmail.com>
2023-02-09 23:16:30 +0800

    remove patches

--- sys/build.rs
+++ sys/build.rs
@@ -5,6 +5,10 @@ use std::{
     process::{Command, Stdio},
 };
 
+fn build_non_wasi() {
+
+}
+
 fn main() {
     #[cfg(feature = "logging")]
     pretty_env_logger::init();
@@ -34,7 +38,7 @@ fn main() {
     }
 
     let src_dir = Path::new("quickjs");
-    let patches_dir = Path::new("patches");
+    
 
     let out_dir = env::var("OUT_DIR").expect("No OUT_DIR env var is set by cargo");
     let out_dir = Path::new(&out_dir);
@@ -60,28 +64,12 @@ fn main() {
         "libbf.c",
     ];
 
-    let mut patch_files = vec![
-        "check_stack_overflow.patch",
-        "infinity_handling.patch",
-        "atomic_new_class_id.patch",
-    ];
-
     let mut defines = vec![
         ("_GNU_SOURCE".into(), None),
         ("CONFIG_VERSION".into(), Some("\"2020-01-19\"")),
         ("CONFIG_BIGNUM".into(), None),
     ];
 
-    if env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows"
-        && env::var("CARGO_CFG_TARGET_ENV").unwrap() == "msvc"
-    {
-        patch_files.push("basic_msvc_compat.patch");
-    }
-
-    if env::var("CARGO_FEATURE_EXPORTS").is_ok() {
-        patch_files.push("read_module_exports.patch");
-        defines.push(("CONFIG_MODULE_EXPORTS".into(), None));
-    }
 
     for feature in &features {
         if feature.starts_with("dump-") && env::var(feature_to_cargo(feature)).is_ok() {
@@ -91,11 +79,6 @@ fn main() {
 
     for file in source_files.iter().chain(header_files.iter()) {
         fs::copy(src_dir.join(file), out_dir.join(file)).expect("Unable to copy source");
-    }
-
-    // applying patches
-    for file in &patch_files {
-        patch(out_dir, patches_dir.join(file));
     }
 
     // generating bindings
@@ -125,24 +108,6 @@ fn feature_to_cargo(name: impl AsRef<str>) -> String {
 
 fn feature_to_define(name: impl AsRef<str>) -> String {
     name.as_ref().to_uppercase().replace('-', "_")
-}
-
-fn patch<D: AsRef<Path>, P: AsRef<Path>>(out_dir: D, patch: P) {
-    let mut child = Command::new("patch")
-        .arg("-p1")
-        .stdin(Stdio::piped())
-        .current_dir(out_dir)
-        .spawn()
-        .expect("Unable to execute patch, you may need to install it: {}");
-    println!("Appliyng patch {}", patch.as_ref().display());
-    {
-        let patch = fs::read(patch).expect("Unable to read patch");
-
-        let stdin = child.stdin.as_mut().unwrap();
-        stdin.write_all(&patch).expect("Unable to apply patch");
-    }
-
-    child.wait_with_output().expect("Unable to apply patch");
 }
 
 #[cfg(not(feature = "bindgen"))]
